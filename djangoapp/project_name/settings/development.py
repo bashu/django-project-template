72928c482bdf58b189a2e4a3467b8da389013dd7
bashu <basil.shubin@gmail.com>
2015-04-21 16:22:29 +0600

    disable CSRF / Cookie settings in ``development`` environment

--- djangoapp/project_name/settings/development.py
+++ djangoapp/project_name/settings/development.py
@@ -5,3 +5,6 @@ DEBUG_PROPAGATE_EXCEPTIONS = True
 ROOT_URLCONF = '{{ project_name }}.urls.development'
 
 WSGI_APPLICATION = '{{ project_name }}.wsgi.development.application'
+
+CSRF_COOKIE_DOMAIN = None
+SESSION_COOKIE_DOMAIN = None
