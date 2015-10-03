5fddc4618db8572f33110ff9492e5f9ab693e06c
Basil Shubin <basil.shubin@gmail.com>
2015-10-03 15:17:04 +0600

    replaced memcached with redis

--- djangoapp/project_name/settings/production.py
+++ djangoapp/project_name/settings/production.py
@@ -4,6 +4,16 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 SERVE_MEDIA = DEBUG
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': '127.0.0.1:6379',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
 ROOT_URLCONF = '{{ project_name }}.urls.production'
 
 WSGI_APPLICATION = '{{ project_name }}.wsgi.production.application'
