ecee0d165d1c66ed4fbf9b3870d0ebb95c8b11eb
bashu <basil.shubin@gmail.com>
2015-11-09 11:26:03 +0500

    re-structured staging settings

--- djangoapp/project_name/settings/staging.py
+++ djangoapp/project_name/settings/staging.py
@@ -1,8 +1,5 @@
-from .base import *
-
-DEBUG_PROPAGATE_EXCEPTIONS = True
+from .production import *
 
 ROOT_URLCONF = '{{ project_name }}.urls.staging'
 
 WSGI_APPLICATION = '{{ project_name }}.wsgi.staging.application'
-
