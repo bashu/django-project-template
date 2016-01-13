57087f2b531805d408d66f3801ad177d43316d17
bashu <basil.shubin@gmail.com>
2016-01-13 09:39:15 +0500

    enable and configuer django-toolbelt

--- djangoapp/project_name/settings/production.py
+++ djangoapp/project_name/settings/production.py
@@ -4,6 +4,9 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 SERVE_MEDIA = DEBUG
 
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost'),
 }
 
 ROOT_URLCONF = '{{ project_name }}.urls.production'
