c94c085de7e4ed9ef411b117be732c30a9926693
Basil Shubin <basil.shubin@gmail.com>
2016-12-13 11:56:17 +0700

    Update base.py

--- djangoapp/project_name/urls/base.py
+++ djangoapp/project_name/urls/base.py
@@ -3,6 +3,8 @@ import re
 from django.conf import settings
 from django.conf.urls import include, url
 
+from django.contrib import admin
+
 urlpatterns = [
     url(settings.ADMIN_URL, include(admin.site.urls)),
     url(r'^robots\.txt$', include('robots.urls')),
